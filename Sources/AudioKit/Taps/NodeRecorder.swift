3e2cfaf776ea30f974a677bbf09cf5e53cc7db74
Long Vu <long.vudai01@gmail.com>
2023-08-06 23:34:52 +0700

    return internalAudioFile

--- Sources/AudioKit/Taps/NodeRecorder.swift
+++ Sources/AudioKit/Taps/NodeRecorder.swift
@@ -48,7 +48,8 @@ open class NodeRecorder: NSObject {
     open var audioFile: AVAudioFile? {
         do {
             if internalAudioFile != nil {
-                closeFile(file: &internalAudioFile)
+//                closeFile(file: &internalAudioFile)
+                return internalAudioFile
             }
             guard let url = recordedFileURL else { return nil }
             return try AVAudioFile(forReading: url)
